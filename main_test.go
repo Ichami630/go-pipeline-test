fe647d9ccf08add5a5d44ea9f69096d43cdeebf5
ichami630 <brandonichami@gmail.com>
2025-04-23 15:25:39 +0100

    solved error

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	if got := sayHello(); got != "Hello, World2!" {
+	if got := sayHello(); got != "Hello, World!" {
 		t.Errorf("sayHello() = %v, want %v", got, "Hello, World!")
 	}
 }
